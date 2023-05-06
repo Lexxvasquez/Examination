aaad6f53ffd73451854abd22d59628096175cf4f
Lexxvasquez <122750837+Lexxvasquez@users.noreply.github.com>
2023-05-06 08:08:31 -0400

    secondretry

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,28 +1,41 @@
 const questions = [
   {
-    Q: 'Question 1',
-    A: ['a','b','c','d'],
+    Q: 'Arrays in JavaScript can be used to store______.',
+    A: ['1. numbers and strings',
+        '2. other arrays',
+        '3.booleans',
+        'all of the above'],
+    C: 'all of the above'
+  },
+  {
+    Q: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    A: ['1. JavaScript',
+    '2. terminal/bash',
+    '3. for loops',
+    '4. console.log'],
     C: 'b'
   },
   {
-    Q: 'Question 2',
-    A: ['a','b','c','d'],
-    C: 'b'
+    Q: 'What does HTML stand for?',
+    A: ['1. Home Tool Markup Language',
+    '2. Hyper Text Markup Language',
+    '3. Hyperlinks and Text Markup Language'],
+    C: 'Hyper Text Markup Language'
   },
   {
-    Q: 'Question 3',
-    A: ['a','b','c','d'],
-    C: 'b'
+    Q: 'What are three ways to insert CSS',
+    A: ['1. External CSS',
+    'Internal CSS','Inline CSS',
+    'All of the above'],
+    C: 'All of the above'
   },
   {
-    Q: 'Question 4',
-    A: ['a','b','c','d'],
-    C: 'b'
-  },
-  {
-    Q: 'Question 5',
-    A: ['a','b','c','d'],
-    C: 'b'
+    Q: 'The Condition in an if / else statment is enclosed with _____.',
+    A: ['1. quotes',
+    '2. curly brackets',
+    '3. Parenthesis',
+    '4. square brackets'],
+    C: 'Paranthesis'
   }
 ];
 
