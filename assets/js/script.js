34497b141f9d1052c2edb930d9599ec2d1d56411
Lexxvasquez <122750837+Lexxvasquez@users.noreply.github.com>
2023-05-06 21:13:40 -0400

    fourthentry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,30 @@
+let qI = 0;
+let time = -3;
+const showQuestions = () => {
+  let { Q, A, C } = questions[qI];
+  main.innerHTML = `<h2>${Q}</h2><div id='answers'></h2>`;
 
+  A.forEach((ans) => {
+    answers.innerHTML += `<button onclick="handleAns('${ans}')">${ans}</button>`;
+  });
+};
 start.onclick = () => {
-
-  let qI = 0;
-  let time = -3;
-  
-  let clockId = setInterval(()=> {
+  let clockId = setInterval(() => {
     time--;
 
-    if(time<1) {
+    if (time < 1) {
       clearInterval(clockId);
       time = 0;
     }
-    
+
     clock.innerText = time;
-  },1000)
-
-  const handleAns = answer => {
-    if(answer!= questions[qI].C) time = time-10;
-    qI++;
-    showQuestions();
-  };
-
-  const showQuestions = () => {
-
-    let {Q,A,C} = questions[qI];
-    main.innerHTML = `<h2>${Q}</h2><div id='answers'></h2>`;
-    
-    A.forEach(ans => {
-      answers.innerHTML += `<button onclick="handleAns('${ans}')">${ans}</button>`;
-    });
-  };
+  }, 1000);
 
   showQuestions();
+};
 
-}
+const handleAns = (answer) => {
+  if (answer != questions[qI].C) time = time - 10;
+  qI++;
+  showQuestions();
+};
